9bef7ba70d65da4fc6c0d0a50e0c074c2be7f917
Samuel Mora <93890643+samuelmora01@users.noreply.github.com>
2023-10-27 17:17:19 -0500

    Actualizar updatePwd.php

--- VOTACION/VOTE/updatePwd.php
+++ VOTACION/VOTE/updatePwd.php
@@ -80,7 +80,7 @@
                         $data = trim($data);
                         $data = stripslashes($data);
                         $data = htmlspecialchars($data);
-                        $data = mysql_real_escape_string($data);
+                        $data = mysqli_real_escape_string($data);
                         return $data;
                       } 
 
